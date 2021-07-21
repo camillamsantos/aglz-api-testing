51db2f2a5cede626987440b4f26db305fa3e33ea
camillamsantos <camillamsantos.sm@gmail.com>
2021-07-20 21:46:41 -0300

    update

--- cypress/integration/ping.spec.js
+++ cypress/integration/ping.spec.js
@@ -4,7 +4,7 @@ import assertions from '../support/api/assertions'
 
 
 context('Ping', () => {
-   it('GET Healthcheck', () => {
+    it('GET Healthcheck', () => {
         //requests
         Requests.getPing().then(getPingResponse => {
             assertions.shouldHaveStatus(getPingResponse, 201)
